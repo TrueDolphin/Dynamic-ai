ea9fb5e598c073d69c4dd6dbe155d921a8a75156
James <demonsythe.jm@gmail.com>
2023-02-19 17:46:12 +1100

    Update MissionServer.c

--- Scripts/5_Mission/MissionServer.c
+++ Scripts/5_Mission/MissionServer.c
@@ -29,6 +29,47 @@ modded class MissionServer {
     }
   }
 
+
+  /*
+  Template turned idea for mirroring internal patrols but with map warnings
+  requires navigation so....questionable for native additions.
+
+      #ifdef EXPANSIONMODNAVIGATION
+      CreateMissionMarker("Test Position", player.GetPosition(), 5);
+	    #endif
+
+	#ifdef EXPANSIONMODNAVIGATION
+
+	private ExpansionMarkerModule m_MarkerModule;
+	private ExpansionMarkerData m_ServerMarker;
+
+	void CreateMissionMarker(string markerName, vector location, int timer)
+	{
+		if (!m_MarkerModule){
+      initMarkerModule();
+      if (!m_MarkerModule) {
+        Print("MarkerModule error");
+        return;
+      }
+    }
+		m_ServerMarker = m_MarkerModule.CreateServerMarker( markerName, "Territory", location, ARGB(255, 235, 59, 90), true);
+   GetGame().GetCallQueue( CALL_CATEGORY_SYSTEM ).CallLater( this.RemoveMissionMarker, timer, false, m_ServerMarker.GetUID());
+				 
+	}
+  void initMarkerModule(){
+    CF_Modules<ExpansionMarkerModule>.Get(m_MarkerModule);
+  }
+
+	void RemoveMissionMarker(string uid)
+	{
+		if ( !m_ServerMarker )
+			return;
+		m_MarkerModule.RemoveServerMarker( uid );
+	}
+	#endif
+*/
+
+
   //timer call for varied check loops
   void DynamicTimer() {
     GetGame().GetCallQueue(CALL_CATEGORY_GAMEPLAY).Remove(DynamicTimer);
