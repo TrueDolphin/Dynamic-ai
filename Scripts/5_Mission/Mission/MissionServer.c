0812b0d1d659822fe91eefad1a5e9d87d398ecb3
James <demonsythe.jm@gmail.com>
2023-08-07 17:46:48 +1000

    7/8/2023 core update
    moved to seperate class
    removed unused file
    moved settings

---
+++ Scripts/5_Mission/Mission/MissionServer.c
@@ -0,0 +1,30 @@
+/*
+name:TrueDolphin
+date:7/8/2023
+spatial ai spawns
+
+Moved a bunch to its own class.
+
+*/
+modded class MissionServer {
+  ref const SpatialAI Spatialai = new SpatialAI();
+  ref Spatial_Groups m_Spatial_Groups;
+
+  void MissionServer() {
+    if (GetSpatialSettings().Init() == true) {
+      GetSpatialSettings().PullRef(m_Spatial_Groups);
+      Spatialai.SpatialLoggerPrint("Spatial AI Enabled");
+      if (m_Spatial_Groups.Spatial_MinTimer == 60000) Spatialai.SpatialLoggerPrint("Spatial Debug Mode on");
+      SpatialTimer();
+    }
+  } //constructor
+
+  void SpatialTimer() {
+    Spatialai.SpatialDebugPrint("Spatial::Stack - Start");
+    Spatialai.Spatial_Check(m_Players);
+    Spatialai.SpatialDebugPrint("Spatial::Stack - End");
+    int m_cor = Math.RandomIntInclusive(m_Spatial_Groups.Spatial_MinTimer, m_Spatial_Groups.Spatial_MaxTimer);
+    Spatialai.SpatialLoggerPrint("Next valid check in: " + m_cor + "ms");
+    GetGame().GetCallQueue(CALL_CATEGORY_SYSTEM).CallLater(this.SpatialTimer, m_cor, false);
+  } //timer call for varied check loops #refactored by LieutenantMaster
+};
--- Scripts/5_Mission/Modules/SpatialAI.c
+++ Scripts/5_Mission/Modules/SpatialAI.c
@@ -1,18 +1,12 @@
-/*
-name:TrueDolphin
-date:21/7/2023
-spatial ai spawns
+class SpatialAI {
 
-renaming functions, checking stuff.
-
-*/
-modded class MissionServer {
   const int SZ_IN_SAFEZONE = 0x0001;
   int m_cur = 0;
   int m_Spatial_cur = 0;
   ref array < Man > Spatial_PlayerList = new array < Man > ;
   ref Spatial_Groups m_Spatial_Groups;
   ref Spatial_Players m_Spatial_Players;
+
 
   #ifdef EXPANSIONMODSPAWNSELECTION
     private ExpansionRespawnHandlerModule m_RespawnModule;
@@ -26,29 +20,18 @@ modded class MissionServer {
     } //LM's code altered to a bool in class
   #endif
 
-  void MissionServer() {
-    SpatialLoggerPrint("Spatial AI Date: 21/7/2023");
+  void SpatialAI(){
+    SpatialLoggerPrint("Spatial AI Date: 7/8/2023");
     if (GetSpatialSettings().Init() == true) {
       GetSpatialSettings().PullRef(m_Spatial_Groups);
       SpatialPlayerSettings().PullRef(m_Spatial_Players); 
       InitSpatialTriggers();
-      SpatialLoggerPrint("Spatial AI Enabled");
-      if (m_Spatial_Groups.Spatial_MinTimer == 60000) SpatialLoggerPrint("Spatial Debug Mode on");
-      SpatialTimer();
     }
-  } //constructor
+  }
 
-  void SpatialTimer() {
-    EXPrint("Spatial::Stack - Start");
-    Spatial_Check();
-    EXPrint("Spatial::Stack - End");
-    int m_cor = Math.RandomIntInclusive(m_Spatial_Groups.Spatial_MinTimer, m_Spatial_Groups.Spatial_MaxTimer);
-    SpatialLoggerPrint("Next valid check in: " + m_cor + "ms");
-    GetGame().GetCallQueue(CALL_CATEGORY_SYSTEM).CallLater(this.SpatialTimer, m_cor, false);
-  } //timer call for varied check loops #refactored by LieutenantMaster
 
-  void Spatial_Check() {
-    EXPrint("Spatial::Check - Start");
+  void Spatial_Check(array<Man> m_Players) {
+    SpatialDebugPrint("Spatial::Check - Start");
       // If there are no players in the list, return early.
       if (m_Players.Count() == 0) return;
 
@@ -62,7 +45,7 @@ modded class MissionServer {
       int minAge = m_Spatial_Groups.MinimumAge;
       bool checkMinAge = minAge > 0;
       Spatial_PlayerList = m_Players;
-      for (int i = 0; i < Spatial_PlayerList.Count() + 1; i++)
+      for (int i = 0; i < m_Players.Count(); i++)
       {
         PlayerBase player = PlayerBase.Cast(Spatial_PlayerList.GetRandomElement());
         Spatial_PlayerList.RemoveItem(player);
@@ -91,11 +74,11 @@ modded class MissionServer {
             continue;
           if (i == playerChecks) return;
       }
-    EXPrint("Spatial::Check - End");
+    SpatialDebugPrint("Spatial::Check - End");
   } // Standard loop through the player list, selecting random players and removing them from the list. #refactored by LieutenantMaster
 
   bool Spatial_CanSpawn(PlayerBase player) {
-    EXPrint("Spatial::CanSpawn - Start");
+    SpatialDebugPrint("Spatial::CanSpawn - Start");
     if (player.IsBleeding() && !GetSpatialSettings().Spatial_IsBleeding())
       return false;
     if (player.IsInVehicle() && !GetSpatialSettings().Spatial_InVehicle())
@@ -122,11 +105,11 @@ modded class MissionServer {
     if (m_Spatial_Groups.Points_Enabled != 0 && player.Spatial_CheckSafe())
       return false;
     return true;
-    EXPrint("Spatial::CanSpawn - End");
+    SpatialDebugPrint("Spatial::CanSpawn - End");
   } //checks and overrides
 
   void Spatial_LocalSpawn(PlayerBase player) {
-    EXPrint("Spatial::LocalSpawn - Start");
+    SpatialDebugPrint("Spatial::LocalSpawn - Start");
     if (!player) return;
 
     int m_Groupid = Math.RandomIntInclusive(0, 5000);
@@ -137,28 +120,17 @@ modded class MissionServer {
     Spatial_Group group;
     string faction, loadout, name;
     float chance;
-    if (player.Spatial_CheckZone() == true) {
+    if (player.Spatial_CheckZone()) {
+      group = new Spatial_Group(player.Spatial_MinCount, player.Spatial_MaxCount, 50, player.Spatial_Loadout(), player.Spatial_Faction(), player.Spatial_Name(), player.Spatial_Lootable(), player.Spatial_Chance(), player.Spatial_UnlimitedReload());
       SpawnCount = Math.RandomIntInclusive(player.Spatial_MinCount, player.Spatial_MaxCount);
-      faction = player.Spatial_Faction();
-      loadout = player.Spatial_Loadout();
-      name = player.Spatial_Name();
-      lootable = player.Spatial_Lootable();
-      chance = player.Spatial_Chance();
-      ammo = player.Spatial_UnlimitedReload();
     } else {
       group = Spatial_GetWeightedGroup(m_Spatial_Groups.Group);
       SpawnCount = Math.RandomIntInclusive(group.Spatial_MinCount, group.Spatial_MaxCount);
-      faction = group.Spatial_Faction;
-      loadout = group.Spatial_Loadout;
-      name = group.Spatial_Name;
-      lootable = group.Spatial_Lootable;
-      chance = group.Spatial_Chance;
-      ammo = group.Spatial_UnlimitedReload;
     }
 
     float random = Math.RandomFloat(0.0, 1.0);
-    SpatialDebugPrint("Chance: " + chance + " | random: " + random);
-    if (chance < random) return;
+    SpatialDebugPrint("Chance: " + group.Spatial_Chance + " | random: " + random);
+    if (group.Spatial_Chance < random) return;
 
     if (SpawnCount > 0) {
       if (m_Spatial_Groups.GroupDifficulty == 1) {
@@ -167,13 +139,13 @@ modded class MissionServer {
         if (!PlayerGroup) PlayerGroup = eAIGroup.GetGroupByLeader(player);
         if (PlayerGroup.Count() > 1) SpawnCount += (PlayerGroup.Count() - 1);
       }
-    Spatial_Spawn(player, SpawnCount, faction, loadout, name, lootable, ammo);
+    Spatial_Spawn(player, SpawnCount, group);
     } else {
       SpatialDebugPrint("group/point ai count too low this check");
     }
 
     SpatialDebugPrint("End GroupID: " + m_Groupid);
-    EXPrint("Spatial::LocalSpawn - End");
+    SpatialDebugPrint("Spatial::LocalSpawn - End");
   } //create and stuff.
 
   Spatial_Group Spatial_GetWeightedGroup(array < ref Spatial_Group > groups, array < float > weights = NULL) {
@@ -202,9 +174,9 @@ modded class MissionServer {
     if (GetGame().SurfaceIsSea(pos[0], pos[2]) || GetGame().SurfaceIsPond(pos[0], pos[2])) return false;
     return true;
   } //fixed
-
-  void Spatial_Spawn(PlayerBase player, int bod, string fac, string loa, string GroupName, int Lootable, bool UnlimitedReload) {
-    EXPrint("Spatial::Spawn - Start");
+    //(PlayerBase player, int bod, string fac, string loa, string GroupName, int Lootable, bool UnlimitedReload) 
+  void Spatial_Spawn(PlayerBase player, int bod, Spatial_Group Group) {
+    SpatialDebugPrint("Spatial::Spawn - Start");
     vector startpos, waypointpos;
     if (!Spatial_ValidPos(player, startpos)){
       SpatialLoggerPrint("Valid spawnpos not found. not spawning.");
@@ -221,18 +193,18 @@ modded class MissionServer {
     mindistradius = 0;
     maxdistradius = 1200;
     despawnradius = 1200;
-    auto dynPatrol = eAISpatialPatrol.CreateEx(startpos, waypoints, behaviour, loa, bod, m_Spatial_Groups.CleanupTimer + 500, m_Spatial_Groups.CleanupTimer - 500, eAIFaction.Create(fac), eAIFormation.Create(Formation), player, mindistradius, maxdistradius, despawnradius, 2.0, 3.0, Lootable, UnlimitedReload);
+    auto dynPatrol = eAISpatialPatrol.CreateEx(startpos, waypoints, behaviour, Group.Spatial_Loadout, bod, m_Spatial_Groups.CleanupTimer + 500, m_Spatial_Groups.CleanupTimer - 500, eAIFaction.Create(Group.Spatial_Faction), eAIFormation.Create(Formation), player, mindistradius, maxdistradius, despawnradius, 2.0, 3.0, Group.Spatial_Lootable, Group.Spatial_UnlimitedReload);
     if (dynPatrol) {
-      dynPatrol.SetGroupName(GroupName);
+      dynPatrol.SetGroupName(Group.Spatial_Name);
       dynPatrol.SetSniperProneDistanceThreshold(0.0);
       dynPatrol.SetHunted(player);
     }
-    Spatial_message(player, m_Spatial_Groups.MessageType, bod, fac, loa);
-    EXPrint("Spatial::Spawn - End");
-  } //Spatial_Spawn(player, SpawnCount, faction, loadout, unlimitedreload)
+    Spatial_message(player, m_Spatial_Groups.MessageType, bod, Group.Spatial_Faction, Group.Spatial_Loadout);
+    SpatialDebugPrint("Spatial::Spawn - End");
+  } //Spatial_Spawn(player, SpawnCount, group)
 
   void InitSpatialTriggers() {
-        EXPrint("Spatial::Triggers - Start");
+        SpatialDebugPrint("Spatial::Triggers - Start");
     if (m_Spatial_Groups.Points_Enabled == 1 || m_Spatial_Groups.Points_Enabled == 2) {
       SpatialLoggerPrint("points Enabled");
       foreach(Spatial_Point points: m_Spatial_Groups.Point) {
@@ -258,7 +230,7 @@ modded class MissionServer {
     } else {
       SpatialLoggerPrint("Locations Disabled");
     }
-        EXPrint("Spatial::Triggers - End");
+        SpatialDebugPrint("Spatial::Triggers - End");
   } //trigger zone initialisation
 
   void Spatial_message(PlayerBase player, int msg_no, int SpawnCount, string faction, string loadout) {
@@ -300,4 +272,4 @@ modded class MissionServer {
     if (m_Spatial_Groups.Spatial_MinTimer == 60000)
       GetExpansionSettings().GetLog().PrintLog("[Spatial Debug] " + msg);
   } //expansion debug print
-};
+}
